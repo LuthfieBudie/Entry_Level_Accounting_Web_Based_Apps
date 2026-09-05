9d4c3fbcfa0b5c362c067183205cb89741ea85a5
LuthfieBudie <luthfiebudieandika@gmail.com>
2026-09-05 15:55:51 +0700

    Delete views directory

--- views/dashboard/dashboard.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('logoutBtn').addEventListener('click', function(e) {
-    e.preventDefault();
-    localStorage.removeItem('user'); 
-
-    window.location.href = '/views/auth/login.html';
-});
